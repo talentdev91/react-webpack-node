c651dbc40f06377cf867e4869df23a3b05afbacb
goldsun27 <todayhonest27@gmail.com>
2016-05-16 17:28:54 -0300

    import controllers in auth callback

--- server/db/mongo/passport/salesforce.js
+++ server/db/mongo/passport/salesforce.js
@@ -1,6 +1,10 @@
+import controllers from '../controllers';
+
 export default (token, refreshToken, profile, done) => {
   console.log('token: ', token);
   console.log('refreshToken: ', refreshToken);
   console.log('profile: ', profile);
+  console,log('controllers: ', controllers);
+
   return done(null, profile);
 };
